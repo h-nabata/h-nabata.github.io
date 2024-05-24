215d9702b967de4f421c89fb34ba753821f18226
h-nabata <64454988+h-nabata@users.noreply.github.com>
2024-05-25 01:46:22 +0900

    Update molecule-visualizer_main.js

--- molecule-visualizer/molecule-visualizer_main.js
+++ molecule-visualizer/molecule-visualizer_main.js
@@ -1,5 +1,6 @@
 function renderMolecule() {
     const xyzData = document.getElementById('xyz_input').value;
+    console.log("Input XYZ data:", xyzData);  // デバッグ用
     const viewer = $3Dmol.createViewer("viewer", {
         defaultcolors: $3Dmol.rasmolElementColors
     });
@@ -7,4 +8,5 @@ function renderMolecule() {
     viewer.setStyle({}, {stick: {}});
     viewer.zoomTo();
     viewer.render();
+    console.log("Molecule rendered");  // デバッグ用
 }
